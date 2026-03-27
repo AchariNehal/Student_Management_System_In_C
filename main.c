0e66bed9c4c2e280725bb981e1125b2c775f0a3a
AchariNehal <nehalachari25@gmail.com>
2026-03-27 17:50:29 +0530

    step 1:Basic Menu Created

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    while(1){
+        int choice;
+        printf("......MENU......\n");
+        printf("1.Add Students \n");
+        printf("2.Delete Students\n");
+        printf("3.Exit\n");
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:printf("Add Student selected\n");
+                    break;
+            case 2:printf("Delete Student selected\n");
+                    break;
+            case 3:exit(0);
+            default: printf("Invalid choice.Please try again!!\n");
+        }
+
+    }
+    return 0;
+}
